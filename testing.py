0fbf0cec9c6f39feb48c424b1501b5bddb993ecb
Bilal Haneef <Bilalhanif848@gmail.com>
2023-07-08 01:55:22 +0500

    commiting changes

--- testing.py
+++ testing.py
@@ -1,11 +1,12 @@
 import os
-from constants import openai_key
 from langchain.llms import OpenAI
 import streamlit as st
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
 
-os.environ['OPENAI_API_KEY'] = openai_key
+
+os.environ['OPENAI_API_KEY'] = 'LMAO'
 llm = OpenAI(temperature=0.8)
 
 # st.title('Search Interview Fields')
@@ -15,36 +16,36 @@ st.markdown(
 )
 
 first_question = PromptTemplate(
-    input_variables = ['skill_name'] ,
-    template = 'Generate 3 difficult questions about {skill_name} skill')
-
-# first_answer = PromptTemplate(
-#     input_variables = ['skill_name'] ,
-#     template = 'Generate 3 difficult questions about {skill_name} skill')
-
+    input_variables = ['skill_qs'] ,
+    template = 'Generate a difficult questions about {skill_qs} skill')
 
 # OPENAI LLMS
 # tempratture param controls how balance the reponse should be
-chain = LLMChain(llm=llm , prompt=first_question,verbose=True) 
+question = LLMChain(llm=llm , prompt=first_question,verbose=True) 
 
-qs = chain.run('tensorflow')
-qs = qs.strip().split('\n')
+qs = question.run('Artificial Neural Network')
+# qs = qs.strip().split('\n')
 
+
+
+first_answer = PromptTemplate(
+    input_variables = ['skill_ans'] ,
+    template = 'Check if {skill_ans} is correct and return a percentage of how correct this answer is')
+
+answer = LLMChain(llm=llm , prompt=first_answer,verbose=True) 
+
+
+st.write(qs)
 input_text = st.text_input('Write your answer here')
 
-for a in qs:
-    print(a)
-    st.write(a)
-    if input_text:
-        st.write(input_text)
+if input_text:
+    st.write(answer.run(input_text))
+
+# for a in qs:
+#     print(a)
+#     st.write(a)
+#     input_text = st.text_input('Write your answer here')
+#     st.write(answer.run(input_text))
+#     # if input_text:
         
-
-# if input_text:
-#     st.write(chain.run(input_text))
-
-
-
-# qs = chain.run('tensorflow')
-# my_list = qs.strip().split('\n')
-# print(my_list)
 
