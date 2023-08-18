55c94ed06d5f3350ef363eb0bf33ac90df9f014a
Bilal Haneef <Bilalhanif848@gmail.com>
2023-08-18 21:58:19 +0500

    changed only one route ,  submit route to return json

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from questions import gpt_qs
 from response_read import generated_qs , speak_qs , get_answer , clear_text_file
 import time
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -47,7 +48,6 @@ def submit_answer():
         return '', 204  # Return a No Content response
 
 
-
 @app.route('/submit',methods = ['POST','GET'])
 def submit():
     if request.method == 'POST':
@@ -66,11 +66,85 @@ def submit():
             "prior_experience": prior_experience,
             "skill & experience": skills
         }
-        print('BEOFRE JSON',response_data)
-        response_data = json.dumps(response_data)
-        print('AFTER JSON',response_data)
 
-    return redirect(url_for('questions',output = response_data))
+    return return jsonify(response_data)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+########################################################### OLD CODE ##################################################################
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# @app.route('/questions/<output>')
+# def questions(output):
+#     output = eval(output)
+#     s_e = output['skill & experience']
+#     for i in s_e:
+#         skill , experience = i.split(',')
+#         gpt_qs(skill,experience)
+#     return redirect(url_for('interview'))
+
+
+
+# @app.route('/interview/',methods = ['POST','GET'])
+# def interview():
+#     question_list = generated_qs()
+
+#     return render_template('interview.html', question_list=question_list)
+
+# @app.route('/submit_answer', methods=['POST'])
+# def submit_answer():
+#     if request.method == 'POST':
+#         data = request.json
+#         question = data.get('question')
+#         user_answer = data.get('userAnswer')
+#         print('Question ------------->', question)
+#         print('Answer ---------------->', user_answer)
+        
+#         # Process the data as needed
+#         response = get_answer(question,user_answer)
+#         print('first question')
+#         print(response)
+#         print('first response')
+#         response_data = {"message": "Answer received successfully"}
+#         return jsonify(response_data)  # Return a JSON response
+#     else:
+#         return '', 204  # Return a No Content response
+
+
+# @app.route('/submit',methods = ['POST','GET'])
+# def submit():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         father_name = request.form['fatherName']
+#         age = int(request.form['age'])
+#         university = request.form['university']
+#         prior_experience = int(request.form['priorExperience'])
+#         skills = request.form.getlist('skill[]')
+
+#         response_data = {
+#             "name": name,
+#             "father_name": father_name,
+#             "age": age,
+#             "university": university,
+#             "prior_experience": prior_experience,
+#             "skill & experience": skills
+#         }
+#         print('BEOFRE JSON',response_data)
+#         response_data = json.dumps(response_data)
+#         print('AFTER JSON',response_data)
+
+#     return redirect(url_for('questions',output = response_data))
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
