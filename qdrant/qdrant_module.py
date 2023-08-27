91d7620d5d70bdd3e1aca7b7e930503e89cf0f5c
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2023-08-27 18:30:37 +0500

    commiting qdrant module for and changes in requirementes

--- qdrant/qdrant_module.py
+++ qdrant/qdrant_module.py
@@ -3,69 +3,34 @@ import numpy as np
 import torch
 import pandas as pd
 from sentence_transformers import SentenceTransformer
-from transformers import ViTImageProcessor, ViTModel
-import cv2
 import numpy as np
-import time
-import os
 
 qdrant = QdrantClient(":memory:")
 
-def upload_qdrant(embedding_path,col_name,emb_size=768):
-    ''' The function uploads embedding in the qdrant based on fixed set of parameters
-    embedding_path : Path of registration car embedding of shape (n,768) in npy format
-    col_name : Any name you want your collection to have     
-    emb_size : Size of your embedding (default = 768)
-     '''
+encoder = SentenceTransformer('all-MiniLM-L6-v2') 
 
-    coll_emb = np.load(embedding_path)
-
+def upload_embd_get_similarity(user_ans,gpt_ans):
     qdrant.recreate_collection(
-	collection_name=col_name,
+	collection_name="m",
 	vectors_config=models.VectorParams(
-		size=emb_size,
-		distance=models.Distance.COSINE
-	))
+		size=encoder.get_sentence_embedding_dimension(),
+		distance=models.Distance.COSINE))
 
-    for idx, embd in enumerate(coll_emb):
-      filename = f"{col_name}_{idx}.npy"
-      payload = {"filename": filename}
+    qdrant.upload_records(
+	collection_name="m",
+	records=[
+		models.Record(
+			id=1,
+			vector=encoder.encode(user_ans).tolist(),
+			payload={'text':user_ans})])
 
-      qdrant.upload_records(
-          collection_name=col_name,
-          records=[
-              models.Record(
-                  id=idx,
-                  vector=embd.tolist(),
-                  payload=payload
-              )
-          ]
-      )
+    hits = qdrant.search(
+	collection_name="m",
+	query_vector=encoder.encode(gpt_ans).tolist(),
+	limit=1)
 
-def do_similarity(target_folder,col_name):
-    '''The function searches given embedding in the given collection 
-    target_folder : Path of a folder having npy embedddings of shape (768,)
-    col_name : Same collection that is set when uploading
-    '''
-    image_paths = []
-    for root, _, filenames in os.walk(target_folder):
-      for filename in filenames:
-        if filename.lower().endswith('.npy'):
-            image_path = os.path.join(root, filename)
-            image_paths.append(image_path)
+    for hit in hits:
+        return hit.score
 
-    d = []
-    for ip in image_paths:
-      test = np.load(ip)
-      hits = qdrant.search(
-          collection_name=col_name,
-          query_vector=test,
-          limit=1)
-      
-      for hit in hits:
-        l = {'Filename':str(hit.payload['filename']+' Vs '+ip),
-            'Score':hit.score}
-        d.append(l)
-    
-    ret = pd.DataFrame(d)
-    return ret
+
+print('ENDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD')
