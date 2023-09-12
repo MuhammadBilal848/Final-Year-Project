91649a2c9bbba818a729fb7bd407373f6a06ede1
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2023-09-13 00:06:40 +0500

    BREAKING CHANGES
    - Added and modified APIs for past evaluations and job listings
    - Added DB handling
    - Added new route for job listings API

--- db/models/evaluation.py
+++ db/models/evaluation.py
@@ -1,22 +1,25 @@
 from peewee import Model, IntegerField, CharField, ForeignKeyField, DateField
-from db.models.user import User 
-from db.db import db 
+from db.models.user import User
+from db.db import db
+
 
 class Evaluation(Model):
     evaluation = IntegerField()
     evaluation_message = CharField(max_length=255)
-    user_id = ForeignKeyField(User, backref='evaluations')
+    user_id = ForeignKeyField(User, backref="evaluations")
     evaluation_date = DateField()
 
     class Meta:
         database = db
-        db_table='evaluation'
+        db_table = "evaluation"
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'evaluation': self.evaluation,
-            'evaluation_message': self.evaluation_message,
-            # 'user': self.user_id.to_dict(), // uncomment agar pora user instance chye with each evaluation
-            'evaluation_date': self.evaluation_date.strftime('%Y-%m-%d'),  # Format the date as a string
+            "id": self.id,
+            "evaluation": self.evaluation,
+            "evaluation_message": self.evaluation_message,
+            "user": self.user_id.to_dict(),
+            "evaluation_date": self.evaluation_date.strftime(
+                "%Y-%m-%d"
+            ),  # Format the date as a string
         }
